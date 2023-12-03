d851a348aec36ba0b56634814f917a3c43e85faa
Vikrant Malla <vikrantmalla999@gmail.com>
2023-12-03 14:18:39 +0545

    feat: add a transaction list component

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
   <main>
     <TotalBalance />
     <IncomeExpense />
+    <TransactionList />
   </main>
 </template>
 
@@ -14,12 +15,14 @@
 import HeaderBar from './components/HeaderBar.vue';
 import TotalBalance from './components/TotalBalance.vue'
 import IncomeExpense from './components/IncomeExpense.vue';
+import TransactionList from './components/TransactionList.vue';
 
 export default {
   components: {
     HeaderBar,
     TotalBalance,
-    IncomeExpense
+    IncomeExpense,
+    TransactionList
   }
 }
 </script>
