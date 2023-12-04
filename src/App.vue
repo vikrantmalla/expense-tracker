11e0755079675808d41d665e1cf2e59801585420
Vikrant Malla <vikrantmalla999@gmail.com>
2023-12-04 11:31:00 +0545

    feat: setup a composition api

--- src/App.vue
+++ src/App.vue
@@ -12,20 +12,10 @@
 </template>
 
 
-<script>
+<script setup>
 import HeaderBar from './components/HeaderBar.vue';
 import TotalBalance from './components/TotalBalance.vue'
 import IncomeExpense from './components/IncomeExpense.vue';
 import TransactionList from './components/TransactionList.vue';
 import AddTransaction from "./components/AddTransaction.vue"
-
-export default {
-  components: {
-    HeaderBar,
-    TotalBalance,
-    IncomeExpense,
-    TransactionList,
-    AddTransaction
-  }
-}
 </script>
