f32ae5699a21f82a7ebe23c783707c7393eae0ee
Vikrant Malla <vikrantmalla999@gmail.com>
2023-12-05 21:40:13 +0545

    feat: add a transaction list in component

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
   <main>
     <TotalBalance />
     <IncomeExpense />
-    <TransactionList />
+    <TransactionList :transactions="transactions" />
     <AddTransaction />
   </main>
 </template>
@@ -18,4 +18,13 @@ import TotalBalance from './components/TotalBalance.vue'
 import IncomeExpense from './components/IncomeExpense.vue';
 import TransactionList from './components/TransactionList.vue';
 import AddTransaction from "./components/AddTransaction.vue"
+
+import { ref } from 'vue';
+
+const transactions = ref(
+  [
+    { id: 1, text: "Flower", amount: -19 },
+    { id: 1, text: "Salary", amount: 500 },
+  ]
+)
 </script>
